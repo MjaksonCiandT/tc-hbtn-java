58aa2f92c007591dab87245b1cb1d454ad95050f
MjaksonCiandT <107194793+MjaksonCiandT@users.noreply.github.com>
2022-06-14 10:06:08 -0300

    Update PrintFormatacaoNumeros.java

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,5 +1,5 @@
 import java.util.Locale;
-public class Main {
+public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
