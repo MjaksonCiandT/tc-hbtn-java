0e552f082a20fd251f9e6814d9ab6bda47895a93
MjaksonCiandT <107194793+MjaksonCiandT@users.noreply.github.com>
2022-06-13 21:38:08 -0300

    Update PrintFloatVariable.java

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
+import java.util.Locale;
 public class Main {
-    public static void main(String[] args) {
-        float pi = 3.1415f;
-        System.out.printf("Valor de pi: %.3f", pi);
-    }
+    public static void main(String[] args) { float pi = 3.14159f;
+        Locale.setDefault(Locale.US);
+        System.out.printf("Valor de pi: %.3f", pi);}
 }
