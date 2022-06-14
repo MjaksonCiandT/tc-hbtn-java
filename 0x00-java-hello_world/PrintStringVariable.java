d9e7ab397810e00d932e99a2de4f419126c286dc
MjaksonCiandT <107194793+MjaksonCiandT@users.noreply.github.com>
2022-06-13 22:30:30 -0300

    Update PrintStringVariable.java

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         String texto = "Holberton School";
-        System.out.printf("%s%n%s%n%s", texto, texto, texto.substring(0, 9));
+        System.out.println( texto + "\n" + texto + "\n" + texto.substring(0, 9));
     }
 }
