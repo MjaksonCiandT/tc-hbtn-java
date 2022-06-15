df7e142131c98395d906b734b2e4b4df8253309f
Maikon Jakson <mjakson@ciandt.com>
2022-06-15 15:32:19 -0300

    Task 1, trilha Java - Java - Classes e Objetos

--- 0x04/encapsulamento/PersonagemGame.java
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -1,4 +1,18 @@
 public class PersonagemGame {
-    public int saudeAtual;
-    public String nome;
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual=saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
 }
