b79e8e33b89d4cd29b6891b4d122d27e75acc093
Maikon Jakson <mjakson@ciandt.com>
2022-06-16 10:45:00 -0300

    Task 2, trilha Java - Java - Classes e Objetos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,44 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int receberCura(int quantidadeDeCura) {
+        int saudeMaximaDoPersonagem = 100;
+        if ((saudeAtual + quantidadeDeCura) > saudeMaximaDoPersonagem) {
+
+            return this.saudeAtual=saudeMaximaDoPersonagem;
+
+        } else {return saudeAtual;}
+
+    }
+
+    public int tomarDano(int quantidadeDeDano) {
+
+        int saudeMínimaDoPersonagem = 0;
+        if ( saudeAtual > saudeMínimaDoPersonagem) {
+
+            this.saudeAtual=saudeAtual - quantidadeDeDano;
+
+        } if (saudeAtual < saudeMínimaDoPersonagem) {
+
+            return this.saudeAtual= saudeMínimaDoPersonagem;
+
+        }else {return saudeAtual;}
+    }
+}
