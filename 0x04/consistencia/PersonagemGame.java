987e52cac465d96875b4479d5623e6b4da36de29
Maikon Jakson <mjakson@ciandt.com>
2022-06-16 22:00:09 -0300

    Task 4, trilha Java - Java - Classes e Objetos

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,66 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int valorSaudeInicial, String nomeInicial) {
+        this.setSaudeAtual();
+    }
+
+    public int getSaudeAtual() {
+        int valorsaudeInicial = 50;
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual() {
+        this.saudeAtual=50;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String setNome(String nome) {
+        return this.nome = "sonic";
+    }
+
+    public String setsaudeDoPersonagem() {
+        String statusMaiorqueZero = "vivo";
+        String statusIgual_A_Zero = "morto";
+
+        if (saudeAtual > 0){
+            this.status=statusMaiorqueZero;
+        }else if (saudeAtual == 0){
+            this.status=statusIgual_A_Zero;
+        }
+        return status;
+    }
+
+    public int receberCura(int quantidadeDeCura) {
+
+        int saudeMaximaDoPersonagem = 100;
+
+        if (saudeAtual + quantidadeDeCura > saudeMaximaDoPersonagem) {
+            this.saudeAtual=saudeMaximaDoPersonagem;
+        }else {
+            this.saudeAtual=saudeAtual+quantidadeDeCura;
+        }
+        return quantidadeDeCura;
+    }
+
+    public int tomarDano(int quantidadeDeDano) {
+
+        int saudeMínimaDoPersonagem = 0;
+        if ( saudeAtual > saudeMínimaDoPersonagem) {
+            this.saudeAtual=saudeAtual - quantidadeDeDano;
+        } if (saudeAtual < saudeMínimaDoPersonagem) {
+            return this.saudeAtual= saudeMínimaDoPersonagem;
+        }else {
+            return saudeAtual;
+        }
+    }
+
+    public String getStatus() {
+        return this.setsaudeDoPersonagem();
+    }
+}
