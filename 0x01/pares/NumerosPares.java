6ff135bc5d1281e7a615191d0bd3f36b09a29d7e
MjaksonCiandT <107194793+MjaksonCiandT@users.noreply.github.com>
2022-06-21 20:48:06 -0300

    Update NumerosPares.java

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,7 +1,7 @@
 public class NumerosPares {
     public static void main(String[] args) {
         for (int cont=0; cont <= 99; cont += 2){
-            if (cont < 99){
+            if (cont < 98){
                 System.out.print(cont + ", ");
             } else {
                 System.out.print(cont + "\n");
