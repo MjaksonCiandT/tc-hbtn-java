53bc59858e827dc1acbae62882f40615c7750d83
Maikon Jakson <mjakson@ciandt.com>
2022-06-21 19:37:57 -0300

    Task 0, 1, 2, 3 Java - Estruturas, Decisão e Repetição

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,7 +1,10 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
         for (int cont=0; cont <= 99; cont++){
-            System.out.printf(cont + ", ");
+            if (cont > 0){
+                System.out.print(", ");
+            }
+            System.out.print(cont);
         }
     }
 }
--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,7 +1,10 @@
 public class NumerosPares {
     public static void main(String[] args) {
         for (int cont=0; cont <= 99; cont += 2){
-            System.out.printf(cont + ", ");
+            if (cont > 0){
+                System.out.print(", ");
+            }
+            System.out.print(cont);
         }
     }
 }
