858ae36f40d056c59c8dd05e1fbd2a0fc6be9762
MjaksonCiandT <107194793+MjaksonCiandT@users.noreply.github.com>
2022-06-21 20:40:40 -0300

    Update Numeros0a99.java

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,10 +1,11 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
         for (int cont=0; cont <= 99; cont++){
-            f (cont > 0){
-                System.out.print(", ");
-            } 
-            System.out.print(cont);
+            if (cont < 99){
+                System.out.print(cont + ", ");
+            } else {
+                System.out.print(cont);
+            }
         }
     }
 }
