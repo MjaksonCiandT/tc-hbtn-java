4dc658865abfb091b44f4702c61cdfd71b5a8566
Maikon Jakson <mjakson@ciandt.com>
2022-06-24 19:52:48 -0300

    Java - Exceções (Parte II)

---
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,71 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+    private String numeracao;
+    private double saldo = 0;
+    private double taxaJurosAnual;
+    private double tarifa = 10.00;
+
+    public ContaBancariaBasica(String numeracao, double taxaJurosAnual){
+        this.numeracao=numeracao;
+        this.taxaJurosAnual=taxaJurosAnual;
+    }
+
+    public String getNumeracao() {
+        return numeracao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getTaxaJurosAnual() {
+        return taxaJurosAnual;
+    }
+
+
+    void depositar(double valor) throws OperacaoInvalidaException {
+        if (valor > 0 ){
+            saldo += valor;
+        }else {
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+        }
+    }
+
+    void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor > 0 && saldo >= valor ){
+            saldo -= valor;
+        }else if(valor < 0){
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+        }else {
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }
+    }
+
+    public double calcularTarifaMensal(){
+        double tarifaFinal;
+        double tarifa = 0.10*this.saldo;
+        if (tarifa<10) {
+            double tarifaMensal = 0.10*this.saldo;
+            tarifaFinal = tarifaMensal;
+        } else {
+            tarifaFinal =10;
+        }
+        return tarifaFinal;
+    }
+
+    public double calcularJurosMensal(){
+        double jurosMensal = getTaxaJurosAnual();
+        if (this.saldo <= 0){
+            jurosMensal=0;
+        }
+            return ((jurosMensal / 12) * this.saldo) / 100;
+    }
+
+    public void aplicarAtualizacaoMensal(){
+        saldo = (this.saldo-calcularTarifaMensal())+calcularJurosMensal();
+    }
+
+
+
+}
