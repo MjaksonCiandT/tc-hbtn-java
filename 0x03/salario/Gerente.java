f3a8ea9d9de50690bcc341f4c884f2803be4ad43
Maikon Jakson <mjakson@ciandt.com>
2022-06-24 21:17:13 -0300

    Task 0 Java - Exceções (Parte II)

--- 0x03/salario/Gerente.java
+++ 0x03/salario/Gerente.java
@@ -8,18 +8,9 @@ public class Gerente extends Empregado{
     public double calcularBonus(Departamento departamento){
         double valorBonus = 0;
         if (departamento.alcancouMeta() == true){
-            return ((salarioFixo * 0.2) + (departamento.getValorAtingidoMeta() * 0.001));
+            return ((salarioFixo * 0.20) + (departamento.getValorMeta() * 0.001));
         }else {
             return valorBonus;
-        }
-    }
-
-    double calcularSalarioTotal(Departamento departamento){
-        double valorsalarioTotal = 0;
-        if (departamento.alcancouMeta() == true){
-            return ((salarioFixo * 0.2) + (departamento.getValorAtingidoMeta() * 0.001) + salarioFixo);
-        }else {
-            return salarioFixo;
         }
     }
 }
