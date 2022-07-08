ab31c88a0013df5f43e7be642c154d4c135af8a4
Maikon Jakson <107194793+MjaksonCiandT@users.noreply.github.com>
2022-07-08 18:08:12 -0300

    Update FolhaPagamento.java

--- Lambda/liquido/FolhaPagamento.java
+++ Lambda/liquido/FolhaPagamento.java
@@ -1,5 +1,5 @@
 public class FolhaPagamento {
     public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario pagamento){
-        return pagamento.calcular(funcionario.Salario());
+        return pagamento.calcular(funcionario.getSalario());
     }
 }
